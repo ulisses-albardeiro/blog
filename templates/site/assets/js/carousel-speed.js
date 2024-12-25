82764a4dc1a8a2c1a869d57451e398cb4d3d943c
Ulisses Albardeiro <ualbardeiro@gmail.com>
2024-12-25 14:37:30 -0300

    blog

--- templates/site/assets/js/carousel-speed.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var carouselElement = document.querySelector('#carouselExampleAutoplaying');
-    var carouselInstance = new bootstrap.Carousel(carouselElement, {
-      interval: 3000 // 3 segundos
-    });
-  });
-  
