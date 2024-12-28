2465e2f9ffae7db116e28319c1f9f266e95004be
Ulisses Albardeiro <ualbardeiro@gmail.com>
2024-12-28 13:08:20 -0300

    Documentação da classe Upload

--- sistema/Biblioteca/Upload.php
+++ sistema/Biblioteca/Upload.php
@@ -3,15 +3,41 @@
 namespace sistema\Biblioteca;
 
 
+/**
+ * Classe independente do sistema.
+ *
+ * Esta classe faz upload de arquivos diversos e que são validados
+ * pela extenção, pelo tipo (definidos internamente) e pelo tamanho (definido no metodo). 
+ * Métodos Disponíveis:
+ * @method __construct(string|null $diretorio)
+ * -arquivo(array $arquivo, string|null $nome, string|null $diretorioFilho, int|null $tamanho): void
+ * -getResultado(): ?string
+ * -getErro(): ?string
+ *
+ * @author Ulisses Alba
+ * @since 28/12/2024
+ */
+
 class Upload
 {
-    public $diretorio;
-    public $arquivo;
-    public $nome;
-    public $diretorioFilho;
-    public $tamanho;
-    public $resultado;
-    public $erro;
+
+    private ?string $diretorio;
+    private array $arquivo;
+    private ?string $nome;
+    private ?string $diretorioFilho;
+    private ?int $tamanho;
+    private ?string $resultado;
+    private ?string $erro;
+
+    /**
+     * Cria uma instância da classe Upload.
+     *
+     * Caso o diretório pai não seja especificado, o valor padrão será 'uploads'.
+     * Se o diretório não existir, ele será criado automaticamente com permissões 0755.
+     *
+     * @param string|null $diretorio Diretório pai onde os arquivos serão movidos. 
+     * Caso não seja fornecido, o valor padrão será 'uploads'.
+     */
 
     public function __construct(string $diretorio = null)
     {
@@ -22,15 +48,40 @@ class Upload
         }
     }
 
+
+    /**
+     * Retorna o resultado da operação em caso de sucesso.
+     *
+     * @return string|null Mensagem do resultado da operação ou null em caso de falha.
+     */
+
     public function getResultado(): ?string
     {
         return $this->resultado;
     }
 
+    /**
+     * Retorna o erro da operação, caso haja.
+     *
+     * @return string|null Mensagem de erro ou null se não houver erro.
+     */
+
     public function getErro(): ?string
     {
         return $this->erro;
     }
+
+    /**
+     * Realiza o upload de um arquivo.
+     *
+     * Valida a extensão, tipo e tamanho do arquivo. Caso o arquivo seja válido, ele será movido
+     * para o diretório de destino.
+     *
+     * @param array $arquivo Arquivo enviado pelo formulário ($_FILES).
+     * @param string|null $nome Nome para o arquivo, caso não seja fornecido, será usado o nome original.
+     * @param string|null $diretorioFilho Diretório filho onde o arquivo será movido. O valor padrão é 'arquivos'.
+     * @param int|null $tamanho Tamanho máximo permitido para o arquivo em megabytes. O valor padrão é 1MB.
+     */
 
 
     public function arquivo(array $arquivo, string $nome = null,  string $diretorioFilho = null, int $tamanho = null): void
@@ -49,17 +100,16 @@ class Upload
             $this->erro = 'exteção de arquivo não permitida, somente ' . implode(" .", $extencoesPermitidas);
         } elseif (!in_array($mimetypeArquivo, $mimetypesPermitidas)) {
             $this->erro = 'Tipo de arquivo não permitido';
-        }elseif($this->arquivo['size'] > $this->tamanho * (1024 * 1024)){
-            $this->erro = 'Arquivo maior que o permitido, tamanho máximo de '. $this->tamanho * (1024 * 1024).'MB e o seu arquivo tem '.$this->arquivo['size'].'MB';
-        }
-         else {
+        } elseif ($this->arquivo['size'] > $this->tamanho * (1024 * 1024)) {
+            $this->erro = 'Arquivo maior que o permitido, tamanho máximo de ' . $this->tamanho * (1024 * 1024) . 'MB e o seu arquivo tem ' . $this->arquivo['size'] . 'MB';
+        } else {
             $this->criarDiretorioFilho();
             $this->renomearArquivo();
             $this->moverArquivo();
         }
     }
 
-    public function renomearArquivo(): void
+    private function renomearArquivo(): void
     {
         $arquivo = strrchr($this->arquivo['name'], '.');
 
@@ -70,7 +120,7 @@ class Upload
         $this->nome = $arquivo;
     }
 
-    public function criarDiretorioFilho(): void
+    private function criarDiretorioFilho(): void
     {
         if (!file_exists($this->diretorio . DIRECTORY_SEPARATOR . $this->diretorioFilho) && !is_dir($this->diretorio . DIRECTORY_SEPARATOR . $this->diretorioFilho)) {
 
@@ -78,7 +128,7 @@ class Upload
         }
     }
 
-    public function moverArquivo(): void
+    private function moverArquivo(): void
     {
         if (move_uploaded_file($this->arquivo['tmp_name'], $this->diretorio . DIRECTORY_SEPARATOR . $this->diretorioFilho . DIRECTORY_SEPARATOR . $this->nome)) {
             $this->resultado = $this->nome;
