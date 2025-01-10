daa15bf84da8612d4dad7b27eda7d2229551ec68
Ulisses Albardeiro <ualbardeiro@gmail.com>
2025-01-09 22:24:22 -0300

    dados no dashboard

--- sistema/Controlador/Admin/AdminDashboard.php
+++ sistema/Controlador/Admin/AdminDashboard.php
@@ -1,21 +1,51 @@
 <?php
 
 namespace sistema\Controlador\Admin;
+
+use sistema\Modelos\CategoriaModelo;
+use sistema\Modelos\PostModelo;
 use sistema\Nucleo\Sessao;
 use sistema\Nucleo\Helpers;
 
 class AdminDashboard extends AdminControlador
 {
-   public function dashboard():void
+   public function dashboard(): void
    {
-    echo $this->template->rendenizar('dashboard.html', []);
+      $visualizacoes = $this->visualizacoes();
+      $posts_ativos = (new PostModelo())->busca('status = 1')->resultado(true);
+      $posts_inativos = (new PostModelo())->busca('status = 0')->resultado(true);
+      $total_categorias = (new CategoriaModelo())->busca()->resultado(true);
+
+      echo $this->template->rendenizar('dashboard.html', [
+         'visualizacoes' => $visualizacoes ?? null,
+         'posts_ativos' => count($posts_ativos) ?? null,
+         'posts_inativos' => isset($posts_inativos) ? count($posts_inativos) : 0,
+         'total_categorias' => isset($total_categorias) ? count($total_categorias) : 0
+      ]);
    }
 
-   public function sair(): void 
+   public function sair(): void
    {
       $sessao = new Sessao;
       $sessao->deletarSessao('usuarioId');
 
       Helpers::redirecionar('admin/login');
    }
+
+   private function visualizacoes(): int
+   {
+      $visualizacoes = (new PostModelo())->busca()->resultado(true);
+
+      if (!isset($visualizacoes->visitas)) {
+         $contagem = 0;
+         foreach ($visualizacoes as $value) {
+            $contagem += $value->visitas;
+         }
+         return $contagem;
+      } 
+
+
+      return 0;
+   }
+
 }
